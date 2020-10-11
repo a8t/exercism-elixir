e6e4f8fc236af44f2757f7bd72ddf9e422368264
Andy <andy8tran@gmail.com>
2020-10-10 23:56:57 -0400

    Fix breaking tests in bob

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,7 +1,29 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      is_silence(input) -> "Fine. Be that way!"
+      is_question(input) and is_allcaps(input) -> "Calm down, I know what I'm doing!"
+      is_question(input) -> "Sure."
+      is_allcaps(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
+  end
+
+  defp is_silence(input) do
+    # a string is silence if only whitespace lies between its beginning and end
+    String.match?(input, ~r/^[\s]*$/)
+  end
+
+  defp is_question(input) do
+    # a string is a question if the last non-whitespace character is a question mark
+    input
+    |> String.trim()
+    |> String.last()
+    |> String.equivalent?("?")
+  end
+
+  defp is_allcaps(input) do
+    # a string is allcaps if it contains letters but doesn't contain lowercase letters
+    String.match?(input, ~r/[[:alpha:]]/) and !String.match?(input, ~r/[[:lower:]]/)
   end
 end
