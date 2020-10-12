05f824fbeab3929a9b69a14a40f762d343d133c2
Andy <andy8tran@gmail.com>
2020-10-12 12:53:27 -0400

    Pass tests in rotation_cipher

--- rotational-cipher/lib/rotational_cipher.ex
+++ rotational-cipher/lib/rotational_cipher.ex
@@ -1,4 +1,6 @@
 defmodule RotationalCipher do
+  @alphabet_length 26
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,5 +10,34 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> to_charlist()
+    |> Enum.map(&rotate_char(&1, shift))
+    |> to_string()
+  end
+
+  defp rotate_char(char, shift) when char >= ?A and char <= ?Z do
+    rotate_letter(char, shift, ?A)
+  end
+
+  defp rotate_char(char, shift) when char >= ?a and char <= ?z do
+    rotate_letter(char, shift, ?a)
+  end
+
+  defp rotate_char(char, _) do
+    char
+  end
+
+  # for uppercase and lowercase letters:
+  # - "zero" the char by subtracting codepoint ?A or ?a respectively
+  # - add the shift
+  # - modulo by @alphabet_length
+  # - add back the original "zeroing" codepoint ?A or ?a
+  defp rotate_letter(char, shift, zeroing_codepoint) do
+    char
+    |> Kernel.-(zeroing_codepoint)
+    |> Kernel.+(shift)
+    |> Kernel.rem(@alphabet_length)
+    |> Kernel.+(zeroing_codepoint)
   end
 end
